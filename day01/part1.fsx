91cddce990cf2fb04883ef5a7f11345dff6f3539
yellowsink <yellowsink@riseup.net>
2022-12-01 12:54:33 +0000

    [d1] bonus F# solutions

---
+++ day01/part1.fsx
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S dotnet fsi --exec
+
+open System
+open System.IO
+
+(new StreamReader(Console.OpenStandardInput())).ReadToEnd().Split("\n\n")
+|> Array.map (fun s ->
+    s.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Array.sumBy int
+  )
+|> Array.max
+|> printfn "%d"
